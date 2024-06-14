afb06ba5f2df959cc776c0166e1bc37781b56299
T25 <106699712+tirso25@users.noreply.github.com>
2024-06-14 20:06:36 +0200

    Update programas.sh

--- programas.sh
+++ programas.sh
@@ -11,10 +11,10 @@ sudo apt update
 sudo apt install code
 
 #JAVA
-JAVA="jdk-21_linux-x64_bin.deb"
-wget https://download.oracle.com/java/21/latest/$JAVA
+JDK="jdk-21_linux-x64_bin.deb"
+wget https://download.oracle.com/java/21/latest/$JDK
 sudo apt-get update
 sudo apt-get install -f
-sudo dpkg -i $JAVA
+sudo dpkg -i $JDK
 echo "export PATH=$PATH:/usr/lib/jvm/jdk-21-oracle-x64/bin" >> ~/.bashrc
 javac --versionS
